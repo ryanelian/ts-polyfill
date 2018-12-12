8be181923f0497b063e3fa2dc161d7ca49581f52
Ryan Elian <ryan.elian.cs@gmail.com>
2018-12-12 14:30:38 +0700

    Update packages but not releasing new version.

--- dist/ts-polyfill.js
+++ dist/ts-polyfill.js
@@ -19,7 +19,7 @@ var tsPolyfill = (function () {
 	});
 
 	var _core = createCommonjsModule(function (module) {
-	var core = module.exports = { version: '2.5.7' };
+	var core = module.exports = { version: '2.6.0' };
 	if (typeof __e == 'number') __e = core; // eslint-disable-line no-undef
 	});
 	var _core_1 = _core.version;
